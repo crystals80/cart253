ee9f04905cb1cdf02ff3e1a3a3690e521f953022
Lam Ky Anh Do <faleanh@hotmail.com>
2021-09-10 00:33:44 -0400

    Added & experimented with colours

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -21,32 +21,70 @@ Description of setup
 */
 function setup() {
   createCanvas(500,500);
+  /*color: red,green,blue,alpha aka transparency)*/
   background(190,84,224);
   /*rect(x, y, w, h,)*/
 
+  stroke(227,227,227);
+
+
+
+  stroke(227,227,227);
+  rectMode(CENTER);
+  fill(255,255,0,100);
+  rect(450,450,100,100);
+  fill(255,255,0,175);
+  rect(450,450,75,75);
+  fill(255,255,0,255);
+  rect(450,450,50,50);
 
   ellipseMode(CORNER);
+  fill('#EEBAFF');
   ellipse(-50,-50,500,500);
+  fill('#D883F2');
   ellipse(-50,-50,400,400);
+  fill('#BE54E0');
   ellipse(-50,-50,300,300);
+  fill("#A92ED2");
   ellipse(-50,-50,200,200);
+  fill('#950BC2');
   ellipse(-50,-50,100,100);
 
+  noStroke();
   ellipseMode(CORNER);
-  ellipse(250,250,100,100);
-  ellipse(250,250,80,80);
-  ellipse(250,250,60,60);
-  ellipse(250,250,40,40);
-  ellipse(250,250,20,20);
+  fill('rgba(255,255,32,100)');
+  ellipse(242,325,100,100);
+  fill('rgba(230,230,0,90)');
+  ellipse(242,325,80,80);
+  fill('rgba(191,191,25,75)');
+  ellipse(242,325,60,60);
+  fill('rgba(140,140,18,55)');
+  ellipse(242,325,40,40);
+  fill('rgba(77,77,10,30)');
+  ellipse(242,325,20,20);
 
-  rectMode(CENTER);
-  rect(250,250,100,100);
-  rect(250,250,75,75);
-  rect(250,250,50,50);
+  noStroke();
+  ellipseMode(CORNER);
+  fill(127,0,200);
+  ellipse(325,243,100,100);
+  fill(137,0,210);
+  ellipse(325,243,80,80);
+  fill(147,0,220);
+  ellipse(325,243,60,60);
+  fill(157,0,230);
+  ellipse(325,243,40,40);
+  fill(167,0,240);
+  ellipse(325,243,20,20);
 
-  point(250,250);
+  stroke(227,227,227);
+  line(375,375,400,400);
+
+  stroke('yellow');
+  strokeWeight(10);
+  point(376,376);
+
   /*rect(10,10,240,240);*/
-  line(0,0,500,500);
+
   /*line(500,0,0,500);
   ellipse(250,250,100,100);*/
 
