24992904df853d3bcb8cdf751af59f3e09af1a1f
Lam Ky Anh Do <faleanh@hotmail.com>
2021-09-09 21:50:19 -0400

    Drew & experimented with shapes

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -1,9 +1,8 @@
 /**
-Title of Project
-Author Name
+Drawing Shapes
+Lam Ky Anh Do
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Template description of project!
 */
 
 "use strict";
@@ -21,6 +20,35 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(500,500);
+  background(190,84,224);
+  /*rect(x, y, w, h,)*/
+
+
+  ellipseMode(CORNER);
+  ellipse(-50,-50,500,500);
+  ellipse(-50,-50,400,400);
+  ellipse(-50,-50,300,300);
+  ellipse(-50,-50,200,200);
+  ellipse(-50,-50,100,100);
+
+  ellipseMode(CORNER);
+  ellipse(250,250,100,100);
+  ellipse(250,250,80,80);
+  ellipse(250,250,60,60);
+  ellipse(250,250,40,40);
+  ellipse(250,250,20,20);
+
+  rectMode(CENTER);
+  rect(250,250,100,100);
+  rect(250,250,75,75);
+  rect(250,250,50,50);
+
+  point(250,250);
+  /*rect(10,10,240,240);*/
+  line(0,0,500,500);
+  /*line(500,0,0,500);
+  ellipse(250,250,100,100);*/
 
 }
 
