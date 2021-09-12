a2e63121c3066600fc84b7d1e2b3636b047f40f1
Lam Ky Anh Do <faleanh@hotmail.com>
2021-09-11 22:40:36 -0400

    Activity 02: Drew an alien

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -1,9 +1,8 @@
 /**
-Draw an Alien
+Activity 2: Draw an Alien
 Lam Ky Anh Do
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Draw an alien on canvas
 */
 
 "use strict";
@@ -21,6 +20,41 @@ function preload() {
 Description of setup
 */
 function setup() {
+
+  // Create background
+  createCanvas(640,480);
+  background('pink');
+  noStroke();
+
+  // Draw the body
+  fill('gray');
+  ellipse(320,480,300,200);
+
+  // Draw the head
+  fill(100);
+  ellipse(320,240,250,400);
+
+  // Draw the eyes
+  fill(0);
+  ellipse(250,240,80,200);
+  ellipse(390,240,80,200);
+
+  // Draw the nostrils
+  ellipse(300,350,10,10);
+  ellipse(340,350,10,10);
+
+  // Draw the mouth
+  ellipse(320,420,25,25);
+
+
+
+  /*stroke("red");
+  strokeWeight(5);
+  rectMode(CENTER);
+  rect(320,390,100,25);*/
+
+
+
 
 }
 
