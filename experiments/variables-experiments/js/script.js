2afc7502d1c4c7c9036833890d0bb6d390ae3c82
Lam Ky Anh Do <faleanh@hotmail.com>
2021-09-23 11:41:05 -0400

    Started variables eperiments folder

---
+++ experiments/variables-experiments/js/script.js
@@ -0,0 +1,38 @@
+/**
+Drawing Shapes
+Lam Ky Anh Do
+
+Experimenting with p5's drawing and colour functions
+**/
+
+"use strict";
+
+
+/**
+Description of preload
+**/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+**/
+function setup() {
+
+  // Create background
+  createCanvas(500, 500);
+  /*color: red,green,blue,alpha aka transparency)*/
+  background(190, 84, 224);
+  /*rect(x, y, w, h,)*/
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
