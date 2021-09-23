d869e6abcb7e9fe285174d1fbc8d78a5a95d8dfa
Lam Ky Anh Do <faleanh@hotmail.com>
2021-09-23 13:01:31 -0400

    Learned about Javacript Objects

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -2,24 +2,34 @@
 Learning how to use variables
 Lam Ky Anh Do
 
-Experimenting with p5's to use variables
+Experimenting with p5's to use variables to create interactivity of shapes, colours and objects
 **/
 
 "use strict";
 
-// Declare variables
+// Declare variables in Creating & Changing Variables
 let backgroundShade = 0;
 //let circleX = 250;
-let circleX = 500;
-let circleY = 250;
-let circleSize = 200;
+//let circleX = 500;
+//let circleX = 0
+//let circleY = 250;
+//let circleSize = 200;
 //let circleSpeed = 2;
 // If you want circleSpeed to run backwards, it needs a negative symbol OR you can leave it be here and change symbol in the draw function
-let circleSpeed = 2;
-let circleAcceleration = 0.25;
+//let circleSpeed = 2;
+//let circleAcceleration = 0.25;
 
 // Undefined makes it a default but causes trouble
 //let circleSize = undefined;
+
+// Declare variables in Intro to Javascript Objects
+// Instead of writing multiples "let," you can write it like this
+let circle = {
+  x: 0,
+  y: 250,
+  size: 200,
+  speed: 2
+}
 
 /**
 Exercises 1,2,3,4,5
@@ -63,7 +73,7 @@ rect(mouseX, mouseY, 100, 100);
 //rect(250, 250, mouseX, mouseY);*/
 
 /**
-Creating & Changing Variables
+Creating & Changing Variables + Intro to Javascript Objects
 **/
 function draw() {
 
@@ -73,7 +83,6 @@ function draw() {
   background(0);
   // Background changes according to declaration
   background(backgroundShade);
-
   // Change backgroundShade
   backgroundShade = backgroundShade + 1;
 
@@ -102,11 +111,14 @@ function draw() {
   //circleX = circleX + circleSpeed;
   //circleX = circleX - circleSpeed;
   // Shortcut for previous
-  circleX -= circleSpeed;
+  //circleX -= circleSpeed;
   // Accelerate circleSpeed
-  circleSpeed = circleSpeed + circleAcceleration;
+  //circleSpeed = circleSpeed + circleAcceleration;
 
   // Ellipse changes according to declaration
-  ellipse(circleX, circleY, circleSize);
+  //ellipse(circleX, circleY, circleSize);
+
+  circle.x += circle.speed;
+  ellipse(circle.x, circle.y, circle.size);
 
 }
