533309b7517ffcba1c920e5401ecdfbd1b3404a3
Lam Ky Anh Do <faleanh@hotmail.com>
2021-09-23 12:00:38 -0400

    Learned about using built-in variables

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -1,8 +1,8 @@
 /**
-Drawing Shapes
+Playing with variables to move mouse and colours
 Lam Ky Anh Do
 
-Experimenting with p5's drawing and colour functions
+Experimenting with p5's variables for mouse and colours
 **/
 
 "use strict";
@@ -21,10 +21,11 @@ Description of setup
 **/
 function setup() {
 
-  // Create background
-  createCanvas(500, 500);
+  // Create canvas
+  //createCanvas(500, 500);
+  //createCanvas(800, 500);
+  createCanvas(windowWidth, windowHeight);
   /*color: red,green,blue,alpha aka transparency)*/
-  background(190, 84, 224);
   /*rect(x, y, w, h,)*/
 
 }
@@ -34,5 +35,24 @@ function setup() {
 Description of draw()
 */
 function draw() {
+
+  background(255, 220, 220);
+  // Limited colours
+  //background(mouseX, mouseY, 0)
+  // A bit more colours
+  background(mouseX, mouseY, mouseY / 2);
+  rectMode(CENTER);
+  //rect(250, 250, 100, 100);
+
+  // Rect stays in mid of any canvas
+  rect(width / 2, height / 2, 100, 100);
+
+  // Mouse with rect following it
+  rect(mouseX, mouseY, 100, 100);
+  // Mouse with rect following opp side
+  //rect(mouseY, mouseX, 100, 100);
+  // Mouse moves and changes size of rect
+  //rect(250, 250, mouseX, mouseY);
+
 
 }
