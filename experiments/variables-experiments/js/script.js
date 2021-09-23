38abaaea1185551a5f482b12e20b333a361c8308
Lam Ky Anh Do <faleanh@hotmail.com>
2021-09-23 12:26:40 -0400

    Learned about declaring variables

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -7,52 +7,88 @@ Experimenting with p5's variables for mouse and colours
 
 "use strict";
 
+// Declare variables
+let backgroundShade = 0;
+let circleX = 250;
+let circleY = 250;
+let circleSize = 200;
+
+// Undefined makes it a default but causes trouble
+//let circleSize = undefined;
 
 /**
-Description of preload
-**/
-function preload() {
-
-}
-
-
-/**
-Description of setup
+Exercises 1,2,3,4,5
 **/
 function setup() {
 
   // Create canvas
-  //createCanvas(500, 500);
+  createCanvas(500, 500);
   //createCanvas(800, 500);
-  createCanvas(windowWidth, windowHeight);
-  /*color: red,green,blue,alpha aka transparency)*/
-  /*rect(x, y, w, h,)*/
+  //createCanvas(windowWidth, windowHeight);
+
+
+}
+
+/**
+Intro to Variables
+**/
+//function draw() {
+
+/*background(255, 220, 220);
+
+// Limited colours
+//background(mouseX, mouseY, 0)
+
+// A bit more colours
+background(mouseX, mouseY, mouseY / 2);
+
+rectMode(CENTER);
+//rect(250, 250, 100, 100);
+
+// Rect stays in mid of any canvas
+rect(width / 2, height / 2, 100, 100);
+
+// Mouse with rect following it
+rect(mouseX, mouseY, 100, 100);
+
+// Mouse with rect following opp side
+//rect(mouseY, mouseX, 100, 100);
+
+// Mouse moves and changes size of rect
+//rect(250, 250, mouseX, mouseY);*/
+
+/**
+Creating Variables
+**/
+//function draw() {
+
+// Creating variables
+
+// Static background colour
+/*background(0);
+// Background changes according to declaration
+background(backgroundShade);
+
+//ellipse(250, 250, 400, 400);
+//ellipse(250, 250, 300, 300);
+//ellipse(250, 250, 200, 200);
+//ellipse(250, 250, 100, 100);
+
+// Static shape
+ellipse(250, 250, 100);
+// Shape changes according to declaration
+ellipse(250, 250, circleSize);
+// Ellipse changes according to declaration
+ellipse(circleX, circleY, circleSize);*/
+
+function draw() {
 
 }
 
 
-/**
-Description of draw()
-*/
-function draw() {
 
-  background(255, 220, 220);
-  // Limited colours
-  //background(mouseX, mouseY, 0)
-  // A bit more colours
-  background(mouseX, mouseY, mouseY / 2);
-  rectMode(CENTER);
-  //rect(250, 250, 100, 100);
 
-  // Rect stays in mid of any canvas
-  rect(width / 2, height / 2, 100, 100);
 
-  // Mouse with rect following it
-  rect(mouseX, mouseY, 100, 100);
-  // Mouse with rect following opp side
-  //rect(mouseY, mouseX, 100, 100);
-  // Mouse moves and changes size of rect
-  //rect(250, 250, mouseX, mouseY);
 
 
 }
