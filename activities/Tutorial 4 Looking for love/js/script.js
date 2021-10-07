f941dc6b2dc9341bdedc4c3f1d178a64b41eeb02
Lam Ky Anh Do <faleanh@hotmail.com>
2021-10-07 13:42:52 -0400

    Started Tutorial 5: Looking for love

---
+++ activities/Tutorial 4 Looking for love/js/script.js
@@ -0,0 +1,23 @@
+/**
+Tutorial 4: Looking for Love
+Lam Ky Anh Do
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+
+function preload() {
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
