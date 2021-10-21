942e381eeeabd0ed7733f1947e5913af077523df
Lam Ky Anh Do <faleanh@hotmail.com>
2021-10-21 00:55:48 -0400

    Started to experimenting with arrays and intermediate functions

---
+++ experiments/arrays-experiments/js/intermediate-functions.js
@@ -0,0 +1,32 @@
+/**
+Learning about intermediate functions
+Lam Ky Anh Do
+
+Experimenting with p5 on how to use more sophisticated functions
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
---
+++ experiments/arrays-experiments/js/intro-to-arrays.js
@@ -0,0 +1,32 @@
+/**
+Learning how to use Arrays
+Lam Ky Anh Do
+
+Experimenting with arrays
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
