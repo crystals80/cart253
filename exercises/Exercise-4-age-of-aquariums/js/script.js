b24d9a93fb9d58ba435545c5896702528e9d7907
Lam Ky Anh Do <faleanh@hotmail.com>
2021-10-26 00:45:58 -0400

    Successful displayed the fishes
    However, since I use images to display the fishes, I have no idea how to make them appear in the array without using random() shown in the "more arrays" video lecture...

--- exercises/Exercise-4-age-of-aquariums/js/script.js
+++ exercises/Exercise-4-age-of-aquariums/js/script.js
@@ -7,26 +7,37 @@ Exercise for class 6: Understanding and playing with arrays
 
 "use strict";
 
-function preload() {
-  //cg.covidImage = loadImage('assets/images/covidgreen.png');
-}
-let school = [];
-let schoolSize = 10; // equal to school.length
+let groupFish1 = [];
+let groupFish2 = [];
+let group = 10;
+let displayFishes1, displayFishes2;
 
-// Our fish
-//let fish1, fish2, fish3, fish4; // replaced by array variable "school"
+// Give fish an appearance
+function preload() {
+  groupFish1[0] = loadImage('assets/images/clown-fish.png');
+  groupFish2[0] = loadImage('assets/images/yellow-fish.png');
+}
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
 
-  for (let i = 0; i < schoolSize; i++) {
-    school[i] = createFish(random(0, width), random(0, height));
+  // Attribute appearance to fish
+  displayFishes1 = random(groupFish1);
+  displayFishes2 = random(groupFish2);
+
+  // Spawn fishes randomly on canvas
+  for (let i = 0; i < group; i++) {
+    groupFish1[i] = createFish(random(0, width), random(0, height));
+  }
+
+  for (let i = 0; i < group; i++) {
+    groupFish2[i] = createFish(random(0, width), random(0, height));
   }
 }
 
-// Create a new Javascript Object describing a fish and return it
+// Create a new Javascript object describing a fish and return it
 function createFish(x, y) {
-  let fish = {
+  let fish1 = {
     x: x,
     y: y,
     size: 50,
@@ -34,44 +45,59 @@ function createFish(x, y) {
     vy: 0,
     speed: 2,
   }
-  return fish;
+  return fish1;
 }
 
 // Moves and display fishes
 function draw() {
   background(0);
 
-  for (let i = 0; i < school.length; i++) {
-    moveFish(school[i]);
+  for (let i = 0; i < groupFish1.length; i++) {
+    moveFish(groupFish1[i]);
   }
-  for (let i = 0; i < school.length; i++) {
-    displayFish(school[i]);
+  for (let i = 0; i < groupFish1.length; i++) {
+    displayFish1(groupFish1[i]);
+  }
+
+  for (let i = 0; i < groupFish2.length; i++) {
+    moveFish(groupFish2[i]);
+  }
+  for (let i = 0; i < groupFish2.length; i++) {
+    displayFish2(groupFish2[i]);
   }
 }
 
 // Chooses whetehr the provided fish changes direction and moves it
-function moveFish(fish) {
+function moveFish(fish1) {
   // Choose whether to change direction
   let change = random(0, 1);
   if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
+    fish1.vx = random(-fish1.speed, fish1.speed);
+    fish1.vy = random(-fish1.speed, fish1.speed);
   }
 
   // Move fish
-  fish.x = fish.x + fish.vx;
-  fish.y = fish.y + fish.vy;
+  fish1.x = fish1.x + fish1.vx;
+  fish1.y = fish1.y + fish1.vy;
 
   // Constrain fish to the canvas
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
+  fish1.x = constrain(fish1.x, 50, width - 50);
+  fish1.y = constrain(fish1.y, 50, height - 50);
 }
 
 // Display the provided fish on the canvas
-function displayFish(fish) {
-  push();
-  fill(200, 100, 100);
+function displayFish1(fish1) {
+
   noStroke();
-  ellipse(fish.x, fish.y, fish.size);
-  pop();
+  image(displayFishes1, fish1.x, fish1.y, fish1.size, fish1.size);
+  imageMode(CENTER);
+
+}
+
+function displayFish2(fish1) {
+
+  noStroke();
+  image(displayFishes2, fish1.x, fish1.y, fish1.size, fish1.size);
+  imageMode(CENTER);
+
 }
