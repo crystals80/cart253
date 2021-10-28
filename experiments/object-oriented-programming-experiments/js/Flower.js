68e2c9061e2d182f638d81294a6997280a3eec8b
Lam Ky Anh Do <faleanh@hotmail.com>
2021-10-28 15:19:16 -0400

    Learned about OOP

---
+++ experiments/object-oriented-programming-experiments/js/Flower.js
@@ -0,0 +1,45 @@
+class Flower {
+  // We will define what a Flower is like in here!
+  constructor() {
+    // We write instructions to set up a Flower here
+    // no need let = flower since constructor is an equivalent
+    // Position and size information
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.size = 50;
+    this.stemLength = 75;
+    this.stemThickness = 10;
+    this.petalThickness = 10;
+    // Color information
+    this.stemColor = {
+      r: 50,
+      g: 150,
+      b: 50
+    };
+    this.petalColor = {
+      r: 200,
+      g: 50,
+      b: 50
+    };
+    this.centreColor = {
+      r: 50,
+      g: 0,
+      b: 0
+    };
+    // No need for return flower since it does it automatically
+  }
+
+  display() {
+    push();
+    // Draw a line for the stem
+    strokeWeight(this.stemThickness);
+    stroke(this.stemColor.r, this.stemColor.g, this.stemColor.b);
+    line(this.x, this.y, this.x, this.y + this.stemLength);
+    // Draw a circle with a heavy outline for the flower
+    strokeWeight(this.petalThickness);
+    fill(this.centreColor.r, this.centreColor.g, this.centreColor.b);
+    stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
+    ellipse(this.x, this.y, this.size);
+    pop();
+  }
+}
