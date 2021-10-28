4b73990309d3436b4dc607465d508a9a72f25003
Lam Ky Anh Do <faleanh@hotmail.com>
2021-10-28 16:42:07 -0400

    Learned to interact objects

---
+++ experiments/object-oriented-programming-experiments/js/4-interacting-objects.js
@@ -0,0 +1,84 @@
+/**
+OOP: Constructors with Parameters
+Lam Ky Anh Do
+
+Experimenting with p5 on how to use objects-oriented programming
+*/
+
+"use strict";
+
+let garden = {
+  // An array to store the individual flowers
+  flowers: [],
+  // Number of flowers in the garden
+  numFlowers: 30,
+  // An array of bees
+  bees: [],
+  // Number of bees in the garden
+  numBees: 7,
+  // The color of the grass (background)
+  grassColor: {
+    r: 120,
+    g: 180,
+    b: 120
+  }
+};
+
+
+function setup() {
+  createCanvas(600, 600);
+
+  // Create flowers by counting up to the number of the flowers
+  for (let i = 0; i < garden.numFlowers; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let size = random(50, 80);
+    let stemLength = random(50, 100);
+    let petalColor = {
+      r: random(100, 255),
+      g: random(100, 255),
+      b: random(100, 255)
+    };
+    // Create a new flower
+    // Don't write constructor because we won'T know which constructor if there are many class
+    let flower = new Flower(x, y, size, stemLength, petalColor);
+    // Add the flower to the array of flowers
+    garden.flowers.push(flower);
+  }
+
+  // Create bees
+  for (let i = 0; i < garden.numBees; i++) {
+    let bee = new Bee(random(0, width), random(0, height));
+    garden.bees.push(bee);
+  }
+}
+
+function draw() {
+  // Display the grass
+  background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+
+  // Loop through all the flowers in the array and display them
+  for (let i = 0; i < garden.flowers.length; i++) {
+    let flower = garden.flowers[i];
+    if (flower.alive) {
+      flower.shrink();
+      flower.display();
+    }
+  }
+
+  for (let i = 0; i < garden.bees.length; i++) {
+    let bee = garden.bees[i];
+    if (bee.alive) {
+      bee.shrink();
+      bee.move();
+      bee.display();
+
+      for (let j = 0; j < garden.flowers.length; j++) {
+        let flower = garden.flowers[j];
+        if (flower.alive) {
+          bee.tryToPollinate(flower);
+        }
+      }
+    }
+  }
+}
