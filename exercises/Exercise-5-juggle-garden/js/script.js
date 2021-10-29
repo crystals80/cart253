c34912c3364c7afc64273fe45d83e38907d1766a
Lam Ky Anh Do <faleanh@hotmail.com>
2021-10-29 11:07:50 -0400

    Chosen theme for E5

--- exercises/Exercise-5-juggle-garden/js/script.js
+++ exercises/Exercise-5-juggle-garden/js/script.js
@@ -1,33 +1,62 @@
 /**
-Title of Project
-Author Name
+Exercise 5 Juggle Garden
+Lam Ky Anh Do
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Exercise for class 7: Understanding and playing with OOP
 */
 
 "use strict";
 
+let force = 0.0025;
+let paddle;
+let balls = [];
+let numBalls = 10;
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  paddle = new Paddle(300, 20);
+
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0, width);
+    let y = random(-400, -100);
+    let ball = new Ball(x, y);
+    balls.push(ball);
+  }
+
+  /*angleMode(DEGREES);
+  //gradient background
+  let c1 = color(78, 234, 242);
+  let c2 = color(18, 110, 25);
+  setGradient(c1, c2);
+  let x = 0
+  let y = 0*/
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(0);
+
+  paddle.move();
+  paddle.display();
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+      ball.gravity(force);
+      ball.move();
+      ball.bounce(paddle);
+      ball.display();
+    }
+  }
 
 }
+
+/*function setGradient(c1, c2) {
+  noFill();
+  for (var y = 0; y < height; y++) {
+    var inter = map(y, 0, height, 0, 1);
+    var c = lerpColor(c1, c2, inter);
+    stroke(c);
+    line(0, y, width, y);
+  }
+}*/
