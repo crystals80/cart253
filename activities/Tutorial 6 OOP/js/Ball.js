ce36ebe4840d2965fe0a34e2be829cecd9a5e863
Lam Ky Anh Do <faleanh@hotmail.com>
2021-10-29 10:26:04 -0400

    Completed Tutorial 6

--- activities/Tutorial 6 OOP/js/Ball.js
+++ activities/Tutorial 6 OOP/js/Ball.js
@@ -24,10 +24,19 @@ class Ball {
 
     this.x += this.vx;
     this.y += this.vy;
+
+    if (this.y - this.size / 2 > height) {
+      this.active = false;
+    }
   }
 
-  bounce() {
-    if (this.y + this.size / 2 >= height) {
+  bounce(paddle) {
+
+    if (this.x > paddle.x - paddle.width * 2 && this.x < paddle.x + paddle.width / 2 && this.y + this.size / 2 > paddle.y - paddle.height / 2 && this.y - this.size / 2 < paddle.y + paddle.height / 2) {
+      // Bounce
+      let dx = this.x - paddle.x;
+      this.vx += map(dx, -paddle.width / 2, paddle.width / 2, -2, 2);
+
       this.vy = -this.vy;
       this.ay = 0;
     }
