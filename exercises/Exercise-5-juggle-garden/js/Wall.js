df745e7847fc7b52640befd926c7157cb0e0bfca
Lam Ky Anh Do <faleanh@hotmail.com>
2021-11-01 00:19:53 -0400

    Finished and Resolved side wall paddle
    I managed to successfully display 2 side wall paddles

--- exercises/Exercise-5-juggle-garden/js/Wall.js
+++ exercises/Exercise-5-juggle-garden/js/Wall.js
@@ -3,16 +3,19 @@ class Wall {
   constructor(w, h, y) {
     this.width = w;
     this.height = h;
-    this.x = width - this.width / 2;
-    this.y = 0;
-
+    this.x1 = 10;
+    this.y1 = 0;
+    this.x2 = width - this.width / 2;
+    this.y2 = 0;
   }
 
   move() {
     if (keyIsDown(DOWN_ARROW)) {
-      this.y -= 10;
+      this.y1 -= 10;
+      this.y2 -= 10;
     } else if (keyIsDown(UP_ARROW)) {
-      this.y += 10;
+      this.y1 += 10;
+      this.y2 += 10;
     }
   }
 
@@ -21,7 +24,8 @@ class Wall {
     fill(255)
     noStroke();
     rectMode(CENTER);
-    rect(this.x, this.y, this.width, this.height);
+    rect(this.x1, this.y1, this.width, this.height);
+    rect(this.x2, this.y2, this.width, this.height);
     pop();
   }
 }
