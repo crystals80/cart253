ca8a856897be34f1770806ba24f10ae695787672
Lam Ky Anh Do <faleanh@hotmail.com>
2021-11-13 23:01:21 -0500

    Started exercise 6

---
+++ exercises/Exercise-6-make-some-noise/js/script.js
@@ -0,0 +1,21 @@
+/**
+Exercise 6: Make some Noise
+Lam Ky Anh Do
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+function preload() {
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
