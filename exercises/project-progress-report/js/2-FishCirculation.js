08badf46db96b9468de787604b441210ffc4b802
Lam Ky Anh Do <faleanh@hotmail.com>
2021-11-18 00:43:35 -0500

    Set up FishCirculation class
    in order to set up sub-Fish classes just like the ones in minigame1 but for a traffic circulation in the sea!

---
+++ exercises/project-progress-report/js/2-FishCirculation.js
@@ -0,0 +1,27 @@
+class FishCirculation {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 40;
+    this.vx = 0;
+    this.vy = 0;
+    this.image = undefined;
+  }
+
+  move() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  wrap() {
+    if (this.x > width) {
+      this.x -= width;
+    } else if (this.x < 0) {
+      this.x += width;
+    }
+  }
+
+  display() {
+
+  }
+}
