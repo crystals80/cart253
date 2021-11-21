aeb5b1ab3daa7ed9fcbbcf473d2c36cf2f8c18a3
Lam Ky Anh Do <faleanh@hotmail.com>
2021-11-20 23:30:16 -0500

    Displayed simulation
    by displaying fish circulating, checking if turtle could get hit by fishes and checking whether turtle/user is alive or dead because it was hit by a fish or reach the top of the canvas

--- exercises/project-progress-report/js/minigame-2.js
+++ exercises/project-progress-report/js/minigame-2.js
@@ -10,4 +10,24 @@ function minigame2() {
   turtle.handleInput();
   turtle.move();
   turtle.display();
+
+  // Display fish circulation and if the fishes would hit the turtle
+  for (let i = 0; i < fishesCirculation.length; i++) {
+    let fishC = fishesCirculation[i];
+    fishC.move();
+    fishC.wrap();
+    fishC.display();
+
+    turtle.checkHit(fishC);
+  }
+
+  // If turtle is hit by one of the fishes then it is game over
+  if (!turtle.alive) {
+    state = `dead`;
+  }
+
+  // If turtle reach the surface (top of canvas) then user have succeeded minigame2
+  if (turtle.y < 0) {
+    state = `success`;
+  }
 }
