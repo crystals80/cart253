e078a7ef9dd2aa8a9216a21dd5dfcc820e435cf4
Lam Ky Anh Do <faleanh@hotmail.com>
2021-11-26 00:27:23 -0500

    Fixed typo attempting to fix link (3rd try)

--- exercises/project-progress-report/js/minigame-3.js
+++ exercises/project-progress-report/js/minigame-3.js
@@ -2,7 +2,7 @@ function minigame3() {
 
   // Display background
   push();
-  image(bg3, 0, 0, windowWidth, windowHeight);
+  image(bg2, 0, 0, windowWidth, windowHeight);
   imageMode(CORNER);
   pop();
 
