53db3282d50602a135544b96adf18e4db18a7e01
Lam Ky Anh Do <faleanh@hotmail.com>
2021-11-26 00:47:32 -0500

    Fixed image file attempting to fix link
    To have it display on github.io (4th try)

--- exercises/project-progress-report/js/minigame-3.js
+++ exercises/project-progress-report/js/minigame-3.js
@@ -2,7 +2,7 @@ function minigame3() {
 
   // Display background
   push();
-  image(bg2, 0, 0, windowWidth, windowHeight);
+  image(bg3, 0, 0, windowWidth, windowHeight);
   imageMode(CORNER);
   pop();
 
