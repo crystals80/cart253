411c8bd967c4d8d0bbea9685fb8baee6a6e2615c
Lam Ky Anh Do <faleanh@hotmail.com>
2021-11-26 01:19:38 -0500

    Fixed resolution of image
    since it was too low quality with jpg

--- exercises/project-progress-report/js/0-in-the-sea.js
+++ exercises/project-progress-report/js/0-in-the-sea.js
@@ -64,7 +64,7 @@ function preload() {
   // Background for minigames
   bg1 = loadImage('assets/images/seabed2.png');
   bg2 = loadImage('assets/images/seabed1.png');
-  bg3 = loadImage('assets/images/antarctica.jpg');
+  bg3 = loadImage('assets/images/antarctica.png');
 }
 
 function setup() {
